e573ad3a35fa292702c040f87fdd9bb4127b8dc1
nibbl3r <kikjous@gmail.com>
2019-03-02 18:00:19 +0100

    Add float2, double2, (u)int2/3/4 and (u)long2/3/4.

--- src/lang/data.lisp
+++ src/lang/data.lisp
@@ -8,52 +8,177 @@
 (defpackage oclcl.lang.data
   (:use :cl)
   (:export ;; Symbol
-           :oclcl-symbol
-           :oclcl-symbol-p
-           ;; Bool
-           :oclcl-bool-p
-           ;; Int
-           :oclcl-int-p
-           ;; Float
-           :oclcl-float-p
-           ;; Double
-           :oclcl-double-p
-           ;; String
-           :oclcl-string-p
-           ;; Float3
-           :float3
-           :make-float3
-           :float3-x
-           :float3-y
-           :float3-z
-           :float3-p
-           :float3-=
-           ;; Float4
-           :float4
-           :make-float4
-           :float4-x
-           :float4-y
-           :float4-z
-           :float4-w
-           :float4-p
-           :float4-=
-           ;; Double3
-           :double3
-           :make-double3
-           :double3-x
-           :double3-y
-           :double3-z
-           :double3-p
-           :double3-=
-           ;; Double4
-           :double4
-           :make-double4
-           :double4-x
-           :double4-y
-           :double4-z
-           :double4-w
-           :double4-p
-           :double4-=))
+   :oclcl-symbol
+   :oclcl-symbol-p
+   ;; Bool
+   :oclcl-bool-p
+   ;; Int
+   :oclcl-int-p
+   ;; Float
+   :oclcl-float-p
+   ;; Double
+   :oclcl-double-p
+   ;; String
+   :oclcl-string-p
+   ;; Float2
+   :float2
+   :make-float2
+   :float2-x
+   :float2-y
+   :float2-z
+   :float2-p
+   :float2-=
+   ;; Float3
+   :float3
+   :make-float3
+   :float3-x
+   :float3-y
+   :float3-z
+   :float3-p
+   :float3-=
+   ;; Float4
+   :float4
+   :make-float4
+   :float4-x
+   :float4-y
+   :float4-z
+   :float4-w
+   :float4-p
+   :float4-=
+   ;; Double2
+   :double2
+   :make-double2
+   :double2-x
+   :double2-y
+   :double2-z
+   :double2-p
+   :double2-=           
+   ;; Double3
+   :double3
+   :make-double3
+   :double3-x
+   :double3-y
+   :double3-z
+   :double3-p
+   :double3-=
+   ;; Double4
+   :double4
+   :make-double4
+   :double4-x
+   :double4-y
+   :double4-z
+   :double4-w
+   :double4-p
+   :double4-=
+   ;; Long2
+   :long2
+   :make-long2
+   :long2-x
+   :long2-y
+   :long2-z
+   :long2-w
+   :long2-p
+   :long2-=
+   ;; Long3
+   :long3
+   :make-long3
+   :long3-x
+   :long3-y
+   :long3-z
+   :long3-w
+   :long3-p
+   :long4-=
+   ;; Long4
+   :long4
+   :make-long4
+   :long4-x
+   :long4-y
+   :long4-z
+   :long4-w
+   :long4-p
+   :long4-=
+   ;; Ulong2
+   :ulong2
+   :make-ulong2
+   :ulong2-x
+   :ulong2-y
+   :ulong2-z
+   :ulong2-w
+   :ulong2-p
+   :ulong2-=
+   ;; Ulong3
+   :ulong3
+   :make-ulong3
+   :ulong3-x
+   :ulong3-y
+   :ulong3-z
+   :ulong3-w
+   :ulong3-p
+   :ulong4-=
+   ;; Ulong4
+   :ulong4
+   :make-ulong4
+   :ulong4-x
+   :ulong4-y
+   :ulong4-z
+   :ulong4-w
+   :ulong4-p
+   :ulong4-=
+   ;; Int2
+   :int2
+   :make-int2
+   :int2-x
+   :int2-y
+   :int2-z
+   :int2-w
+   :int2-p
+   :int2-=
+   ;; Int3
+   :int3
+   :make-int3
+   :int3-x
+   :int3-y
+   :int3-z
+   :int3-w
+   :int3-p
+   :int4-=
+   ;; Int4
+   :int4
+   :make-int4
+   :int4-x
+   :int4-y
+   :int4-z
+   :int4-w
+   :int4-p
+   :int4-=
+   ;; Uint2
+   :uint2
+   :make-uint2
+   :uint2-x
+   :uint2-y
+   :uint2-z
+   :uint2-w
+   :uint2-p
+   :uint2-=
+   ;; Uint3
+   :uint3
+   :make-uint3
+   :uint3-x
+   :uint3-y
+   :uint3-z
+   :uint3-w
+   :uint3-p
+   :uint4-=
+   ;; Uint4
+   :uint4
+   :make-uint4
+   :uint4-x
+   :uint4-y
+   :uint4-z
+   :uint4-w
+   :uint4-p
+   :uint4-=
+   ))
 (in-package :oclcl.lang.data)
 
 
@@ -239,3 +364,50 @@
 (defmethod cffi:translate-from-foreign (value (type double3-c))
   (cffi:with-foreign-slots ((x y z w) value (:struct double4))
     (make-double4 x y z w)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmacro define-lisp-and-foreign-vector-structs (name type cffi-type sizes)
+  `(progn
+     ,@(alexandria:mappend
+	(lambda (n)
+	  (let* ((slots (subseq '(x y z w) 0 n))
+		 (name-n (alexandria:symbolicate name (format nil "~d" n)))
+		 (c-name-n (alexandria:symbolicate name-n '-c))
+		 (constructor (alexandria:symbolicate 'make- name-n))
+                 (slot-vars-a (subseq '(ax ay az aw) 0 n))
+		 (slot-vars-b (subseq '(bx by bz bw) 0 n))
+		 (slot-bindings-a (mapcar (lambda (var slot) `(,var ,slot)) slot-vars-a slots))
+		 (slot-bindings-b (mapcar (lambda (var slot) `(,var ,slot)) slot-vars-b slots)))
+            `((defstruct (,name-n (:constructor ,constructor ,(subseq slots 0 n)))
+		,@(mapcar (lambda (slot) `(,slot nil :type ,type)) slots))
+	      (defun ,(alexandria:symbolicate name-n '-=) (a b)
+		(with-slots ,slot-bindings-a a
+		  (with-slots ,slot-bindings-b b
+		    (and ,@(mapcar (lambda (a b) `(= ,a ,b)) slot-vars-a slot-vars-b)))))
+	      (cffi:defcstruct (,name-n :class ,c-name-n)
+		,@(mapcar (lambda (slot) `(,slot ,cffi-type)) slots))
+	      (defmethod cffi:translate-into-foreign-memory ((value ,name-n) (type ,c-name-n) ptr)
+		(cffi:with-foreign-slots (,slots ptr (:struct ,name-n))
+		  (with-slots ,slot-bindings-a value
+		    (setf ,@(alexandria:mappend (lambda (a b) `(,a ,b)) slots slot-vars-a)))))
+	      (defmethod cffi:expand-into-foreign-memory ((value ,name-n) (type ,c-name-n) ptr)
+		`(cffi:with-foreign-slots (,',slots ,ptr (:struct ,',name-n))
+		   (with-slots ,',slot-bindings-a ,value
+		     (setf ,@',(alexandria:mappend (lambda (a b) `(,a ,b)) slots slot-vars-a)))))
+	      (defmethod cffi:translate-from-foreign (ptr (type ,c-name-n))
+		(cffi:with-foreign-slots (,slots ptr (:struct ,name-n))
+		  (,constructor ,@slots)))
+	      (defmethod cffi:expand-from-foreign (ptr (type ,c-name-n))
+		`(cffi:with-foreign-slots (,',slots ,ptr (:struct ,',name-n))
+		   (,',constructor ,@',slots))))))
+	sizes)))
+
+;; define missing structs
+(define-lisp-and-foreign-vector-structs long (signed-byte 64) :int64 (2 3 4))
+(define-lisp-and-foreign-vector-structs ulong (unsigned-byte 64) :uint64 (2 3 4))
+(define-lisp-and-foreign-vector-structs int (signed-byte 32) :int32 (2 3 4))
+(define-lisp-and-foreign-vector-structs uint (unsigned-byte 32) :uint32 (2 3 4))
+(define-lisp-and-foreign-vector-structs double double-float :double (2))
+(define-lisp-and-foreign-vector-structs float single-float :float (2))
+
